c92109d4d9ea8b81a738034de35e432a7a957e69
tthanhtung92 <55181588+tthanhtung92@users.noreply.github.com>
2022-06-16 22:29:00 +0700

    sửa lại 1 số thứ

--- app/src/main/java/com/example/tennis_booking/HomeActivity.java
+++ app/src/main/java/com/example/tennis_booking/HomeActivity.java
@@ -1,6 +1,7 @@
 package com.example.tennis_booking;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.content.res.ResourcesCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,13 +13,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import com.google.android.gms.maps.SupportMapFragment;
+
 import java.util.ArrayList;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity{
+
+    ArrayList<CourtItem> CourtModelList = new ArrayList<>();
 
     RecyclerView rv;
     RecyclerView rv1;
     EditText edt;
+
     private ArrayList<CourtItem> courtData = new ArrayList<>();
     private ArrayList<CourtItem> courtData1 = new ArrayList<>();
     LinearLayoutManager linearLayoutManager;
@@ -29,10 +35,10 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.fragment_home);
 
         //Thêm dữ liệu vào trong recycle view
-        rv = findViewById(R.id.rvSearch);
+        rv = findViewById(R.id.rvOnDiscount);
         rv1 = findViewById(R.id.rvNearMe);
 
         Resources res = getResources();
@@ -105,6 +111,8 @@ public class HomeActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-    }
 
+        //xử lí click adapter item
+
+    }
 }
