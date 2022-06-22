334213b3bdfd7b5f80157f45ffdf72c9159379fc
SunFlower2000 <52041113+KIBICHAN@users.noreply.github.com>
2022-06-23 00:25:19 +0700

    PaySectionActivity.java

--- app/src/main/java/com/example/tennis_booking/CourtSelectActivity.java
+++ app/src/main/java/com/example/tennis_booking/CourtSelectActivity.java
@@ -1,17 +1,28 @@
 package com.example.tennis_booking;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.annotations.NonNull;
+
 public class CourtSelectActivity extends AppCompatActivity {
 
     Button btn;
-    TextView tv_back_court_select;
+    TextView tv_back_court_select, tv_style;
+    LinearLayout llayout_1;
+    ImageView court1, court2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,9 +42,56 @@ public class CourtSelectActivity extends AppCompatActivity {
         tv_back_court_select.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(CourtSelectActivity.this, TimePickingActivity.class);
+                Intent intent = new Intent(CourtSelectActivity.this, DateBookingActivity.class);
                 startActivity(intent);
             }
         });
+
+        llayout_1 = (LinearLayout) findViewById(R.id.llayout_1);
+        tv_style = (TextView) findViewById(R.id.tv_stype);
+        llayout_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ShowMenu();
+            }
+        });
+
+        court1 = (ImageView) findViewById(R.id.court1);
+        court2 = (ImageView) findViewById(R.id.court2);
+        court1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                court1.setImageResource(R.drawable.select);
+                court2.setImageDrawable(null);
+            }
+        });
+
+        court2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                court2.setImageResource(R.drawable.select);
+                court1.setImageDrawable(null);
+            }
+        });
+    }
+
+    private void ShowMenu(){
+        PopupMenu popupMenu = new PopupMenu(this, llayout_1);
+        popupMenu.getMenuInflater().inflate(R.menu.court_style_menu, popupMenu.getMenu());
+
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.mn_1_vs: tv_style.setText("Court for 1vs1");
+                        break;
+                    case R.id.mn_2_vs: tv_style.setText("Court for 2vs2");
+                        break;
+                }
+                return false;
+            }
+        });
+
+        popupMenu.show();
     }
 }
--- app/src/main/java/com/example/tennis_booking/PaySectionActivity.java
+++ app/src/main/java/com/example/tennis_booking/PaySectionActivity.java
@@ -1,15 +1,80 @@
 package com.example.tennis_booking;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.PopupMenu;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class PaySectionActivity extends AppCompatActivity {
+
+    TextView tv_paymethod;
+    LinearLayout llayout_paymethod;
+    ImageView iv_paymethod;
+    Button booking_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pay_section);
 
+        llayout_paymethod = (LinearLayout) findViewById(R.id.llayout_paymethod);
+        tv_paymethod = (TextView) findViewById(R.id.tv_paymethod);
+        iv_paymethod = (ImageView) findViewById(R.id.iv_paymethod);
+        llayout_paymethod.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ShowMenu();
+            }
+        });
+
+        booking_btn = (Button) findViewById(R.id.booking_btn);
+        booking_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PaySectionActivity.this, SuccessBookingActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void ShowMenu(){
+        PopupMenu popupMenu = new PopupMenu(this, llayout_paymethod);
+        popupMenu.getMenuInflater().inflate(R.menu.payment_method_menu, popupMenu.getMenu());
+
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.ic_momo: tv_paymethod.setText("Momo");
+                        iv_paymethod.setImageResource(R.drawable.momo);
+                        break;
+                    case R.id.ic_zalopay: tv_paymethod.setText("Zalopay");
+                        iv_paymethod.setImageResource(R.drawable.zalopay);
+                        break;
+                    case R.id.ic_mocagrab: tv_paymethod.setText("Moca|Grab");
+                        iv_paymethod.setImageResource(R.drawable.moca);
+                        break;
+                    case R.id.ic_visa: tv_paymethod.setText("Visa card");
+                        iv_paymethod.setImageResource(R.drawable.visa);
+                        break;
+                    case R.id.ic_debitcard: tv_paymethod.setText("Debit card");
+                        iv_paymethod.setImageResource(R.drawable.card);
+                        break;
+                    case R.id.ic_cash: tv_paymethod.setText("Cash");
+                        iv_paymethod.setImageResource(R.drawable.tienne);
+                        break;
+                }
+                return false;
+            }
+        });
+
+        popupMenu.show();
     }
 }
