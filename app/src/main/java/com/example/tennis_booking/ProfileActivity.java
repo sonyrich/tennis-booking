732ff2398843135b818360c7990d736daeae2c77
tthanhtung92 <55181588+tthanhtung92@users.noreply.github.com>
2022-06-27 09:18:46 +0700

    sửa lại 1 chút

--- app/src/main/java/com/example/tennis_booking/ProfileActivity.java
+++ app/src/main/java/com/example/tennis_booking/ProfileActivity.java
@@ -2,13 +2,26 @@ package com.example.tennis_booking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        tv = (TextView) findViewById(R.id.tvProfileBack);
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
